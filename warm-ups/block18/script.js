b9878769b2070489c3334383d6454ae0bbd3b208
Liz Hoppstetter <liz.hoppstetter@fullstackacademy.com>
2024-11-14 17:28:14 -0600

    team warm-up block17

---
+++ warm-ups/block18/script.js
@@ -0,0 +1,34 @@
+// Create a simple program in that models a Library system. 
+//************
+
+
+
+
+// Define a Book class:
+// Each Book should have a title, author, and isCheckedOut property.
+// Create a method called toggleCheckOut that changes the isCheckedOut status.
+class Book{
+
+}
+
+// Define a Library Class
+// The Library should have a books array to store all Book objects.
+// Add a method called addBook that takes a Book object and adds it to the books array.
+// Create a method called listAvailableBooks that logs all the books that are currently not checked out.
+class Library{
+
+}
+
+
+// Use your classes:
+
+// Create an instance of Library.
+
+// Create 3 book objects
+
+// Toggle the checkout status of one book.
+
+// Add the books to instance of library
+
+// Console log all available books in the library.
+
