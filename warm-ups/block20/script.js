f14bf41b98d8691a293c74e661ba44b212f46728
Liz Hoppstetter <liz.hoppstetter@fullstackacademy.com>
2024-11-26 16:20:20 -0600

    Block 20 warmup

---
+++ warm-ups/block20/script.js
@@ -0,0 +1,18 @@
+/*
+
+Observe the index.html page.
+
+It has:
+
+A heading <h1> with the id "mainHeading"
+A paragraph <p> element with the id "description"
+A <div> element with the id "colorBox" that will act as a "box."
+
+Without making any changes to the html, write JavaScript that:
+
+1) Changes the text content of the <h1> element from "Welcome to Color Changer!" to "DOM Manipulation is Fun!"
+2) Changes the text content of the <p> element from "This is a simple DOM manipulation exercise." to "Look at how we changed the elements!"
+3) Changes the background color of the <div> element to light blue and doubles its current size
+4) Adds a new <p> element inside the <div> with the text: "This is a new paragraph inside the box!". */
+
+//Your Code below
