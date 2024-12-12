6187ec1c5515be7e44b268adaf66f828c282f25c
Liz Hoppstetter <liz.hoppstetter@fullstackacademy.com>
2024-12-12 17:50:44 -0600

    learning team warm-up

---
+++ warm-ups/block24/script.js
@@ -0,0 +1,34 @@
+const API_URL = "https://catfact.ninja/"
+
+const state = 
+{
+    fact: {},
+    breeds: []
+};
+
+const $catFactContainer = document.getElementById("cat-fact-container");
+const $catBreedsContainer  = document.getElementById("cat-breeds-container");
+
+//Fetches a random fact from the /fact endpoint
+// and updates the fact object in state
+//then re-renders the UI
+async function getFact(){
+
+}
+
+//Fetches a random fact from the /fact endpoint
+// and updates the fact object in state
+//then re-renders the UI
+async function getBreeds(){
+
+}
+
+//Adds the HTML elements needed to the DOM
+// with data from the javascript state
+function render(){
+    //TODO:  A button and a <p> element to the catFactContainer
+    //TODO:  Divs for each cat breed type, display the Breed, country, coat and pattern 
+
+}
+
+
