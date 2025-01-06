6ada980d57c89f55452b666ac9e5babafbfe311b
Torie Kim <torie.kim@fullstackacademy.com>
2025-01-06 18:17:38 -0500

    add: blocks 24-26

---
+++ block26/workshop-solution/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+  return (
+    <>
+      {selectedContactId ? (
+        <SelectedContact
+          selectedContactId={selectedContactId}
+          setSelectedContactId={setSelectedContactId}
+        />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
+    </>
+  );
+}
