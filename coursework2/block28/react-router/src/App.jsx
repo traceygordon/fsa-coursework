a516d842586c48108a4f69d231155d1db09bf52c
traceygordon <traceyagordon@gmail.com>
2025-01-15 18:07:19 -0500

    completed block28

--- coursework2/block28/react-router/src/App.jsx
+++ coursework2/block28/react-router/src/App.jsx
@@ -1,27 +1,28 @@
 import "./App.css";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Red from "./components/red";
 import Blue from "./components/blue";
 import Home from "./components/Home";
 import Navigation from "./components/Navigation";
+
 
 function App() {
   return (
     <>
       <div id="container">
         <Navigation />
-
-        <div id="main-section">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blue" element={<Blue />} />
           <Route path="/red" element={<Red />} />
         </Routes>
       </div>
-      
-      </div>
 
+      <div id="footer">
+      <Navigation />
      
+
+      </div>
     </>
   );
 }
